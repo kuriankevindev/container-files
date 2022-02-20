fa9458dcfdc347861fc709f534ce3a4c7953e8e0
Kurian Kevin <kuriankevindev@gmail.com>
2022-02-20 12:23:09 +0530

    Removed Sleep and added Loop to Restart App till DB start up

--- employee-spring/hub.docker/1.0.0/start.sh
+++ employee-spring/hub.docker/1.0.0/start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+count=5
+
 if [ $# == 0 ]
 then
-    FLAG="build"
+    FLAG="run"
 else
     FLAG=$1
 fi
@@ -13,20 +15,28 @@ then
     docker container stop employee-app
     echo "Docker container employee-db removing..."
     docker container stop employee-db
+    echo "Volume employee-vol removing..."
+    docker volume rm employee-vol
     echo "Network employee-net removing..."
     docker network rm employee-net
-    exit
 fi
 
-if [ $FLAG == "local" ]
+if [ $FLAG == "run" ]
 then
     echo "Network employee-net creating..."
     docker network create employee-net
+    echo "Volume employee-vol creating..."
+    docker volume create employee-vol
     echo "Docker container employee-db creating..."
-    docker container run --rm -d --name employee-db -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=employee -e MYSQL_PASSWORD=employee -e MYSQL_DATABASE=employeedb -p 3306:3306 --network employee-net mysql:8.0.28
-    sleep 15
+    docker container run --rm -d --name employee-db -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=employee -e MYSQL_PASSWORD=employee -e MYSQL_DATABASE=employeedb -p 3306:3306 -v employee-vol:/var/lib/mysql --network employee-net mysql:8.0.28
     echo "Docker container employee-app creating..."
-    docker container run --rm -d --name employee-app -p 8080:8080 -e DB_HOST=employee-db --network employee-net kuriankevindev/employee-spring:1.0.0
-else
-    docker container run -p 8080:8080 kuriankevindev/employee-spring:1.0.0
+    while [ $count -gt 0 ]
+    do
+        docker container run --rm --name employee-app -p 8080:8080 -e DB_HOST=employee-db --network employee-net kuriankevindev/employee-spring:1.0.0
+        if [ $? == 143 ]
+        then            
+            break
+        fi
+        ((count=count-1))
+    done
 fi
